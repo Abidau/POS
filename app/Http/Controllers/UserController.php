eba6da6e7077f3380c900bcfcc727d5b7049cc6d
Abidau <naufalabidau@gmail.com>
2026-03-08 19:38:05 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,30 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
 
-        $data = UserModel::all(); // Mengambil semua data dari model UserModel
-        return view('user', ['data' => $data]);
+        // $data = UserModel::all(); // Mengambil semua data dari model UserModel
+        // return view('user', ['data' => $data]);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
